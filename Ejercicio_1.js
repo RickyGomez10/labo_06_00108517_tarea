b2b44fa87fe05f9cbbfbdce4a2cef5ce6c920bf7
RickyGomez10 <00108517@uca.edu.sv>
2018-09-29 02:35:54 -0600

    Tarea labo 6

---
+++ Ejercicio_1.js
@@ -0,0 +1,112 @@
+var numero = 0;
+var arreglo = [];
+var ventas = 0;
+var sumaventas = 0;
+var promedioventas = 0;
+while(numero != 6){
+	console.log("Que opcion desea realizar?");
+	console.log("1. Agregar producto");
+	console.log("2.	Modificar stock");
+	console.log("3. Registrar venta y reducir stock");
+	console.log("4. Mostrar promedio de ventas realizadas");
+	console.log("5. Mostrar productos con stock 0");
+	console.log("6. Salir del programa");
+	numero = prompt("Ingrese el numero de una de las opciones");
+	if (numero ==1) {
+		var codigo1 = prompt("Ingrese el codigo del producto");
+		var descrpicion1 = prompt("Ingrese la descripcion del producto");
+		var tipo1 = prompt("Ingrese el tipo del producto");
+		var preciocompra1 = prompt("Ingrese el precio de compra del producto");
+		var precioventa1 = prompt("Ingrese el precio de venta del producto");
+		var stock1 = prompt("Ingrese el stcok del producto");
+		var producto = {
+			codigo: codigo1,
+			descripcion: descrpicion1,
+			tipo: tipo1,
+			preciocompra: preciocompra1,
+			precioventa: precioventa1,
+			stock: stock1
+		}
+		arreglo.push(producto)
+
+	}else if (numero ==2) {
+		var buscar = prompt("Ingrese el codigo del producto que desea modificar");
+		for(let i of arreglo){
+			if (i.codigo == buscar) {
+				console.log("Que desea modificar del producto");
+				console.log("1. Modificar descrpicion");
+				console.log("2. Modificar tipo");
+				console.log("3. Modificar precio de compra del producto");
+				console.log("4. Modificar precio de venta del producto");
+				console.log("5. Modificar el stock del producto");
+				var modificar = prompt("Ingrese la opcion para modificar valor del producto");
+				if(modificar == 1){
+
+				}else if(modificar ==2){
+					var nuevovalor= prompt("Ingrese el nuevo tipo del producto");
+					i.tipo= nuevovalor;
+
+				}else if(modificar ==3){
+					var nuevovalor= prompt("Ingrese el nuevo precio de compra del producto");
+					i.preciocompra = nuevovalor;
+				}
+				else if(modificar ==4){
+					var nuevovalor= prompt("Ingrese el nuevo precio de venta del producto");
+					i.precioventa = nuevovalor;
+				}
+				else if(modificar ==5){
+					var nuevovalor= prompt("Ingrese el nuevo stock del producto");
+					i.stock = nuevovalor;
+				}
+			}
+		}
+
+	}else if (numero ==3) {
+		var buscar= prompt("Ingrese el codigo del producto que desea vender");
+		for(let i of arreglo){
+			if(i.codigo == buscar){
+				if(i.stock == 0){
+					console.log("No puede vender el producto porque usted tiene 0 de stock");
+					break;
+				}else{
+					var vender = prompt("Usted posee: "+i.stock + " del producto: "+i.codigo+", Cuantos desea vender?");
+					var resultadostock = i.stock - vender;
+					if(resultadostock < 0){
+						console.log("No se puede vender la cantidad deseada, usted solo posee: " +i.stock);
+					}else{
+						i.stock = resultadostock;
+						console.log("Producto vendido");
+						ventas = ventas +1;
+					}
+				}
+				
+
+			}
+		}
+
+	}else if (numero ==4) {
+		for(let i of arreglo){
+
+			sumaventas = sumaventas + (i.precioventa-i.preciocompra);
+		}
+		if(ventas == 0){
+			console.log("Usted no tiene promedio de ventas puesto que no ha realizado ninguna venta");
+		}else{
+			promedioventas = sumaventas/ventas;
+			console.log("Su promedio de ganancia por venta es: "+promedioventas);
+		}
+
+
+	}else if (numero ==5) {
+		for(let i of arreglo){
+			if(i.stock == 0){
+				console.log(i);
+			}
+		}
+
+	}else if (numero ==6) {
+		console.log("Usted esta saliendo del programa");
+		break;
+
+	}
+}
---
+++ Ejercicio_2.js
@@ -0,0 +1,79 @@
+var convertor = {
+	convertir: function convertir(medida,medida_de_pieza,medida_a_convertir,tipo_de_medida){
+		if(medida_de_pieza == 'm' && medida_a_convertir =='ft' && tipo_de_medida == 'L'){
+				return medida*3.2808 +' ft';
+				
+		} else if(medida_de_pieza == 'm' && medida_a_convertir =='cm'  && tipo_de_medida == 'L'){
+				return medida/0.01 + ' cm';
+				
+		}else if(medida_de_pieza == 'm' && medida_a_convertir =='km' && tipo_de_medida == 'L'){
+				return medida/1000 +' km';
+				
+			
+		}else if(medida_de_pieza == 'ft' && medida_a_convertir =='m' && tipo_de_medida == 'L'){
+				return medida/3.2808 +' m';
+				
+		}else if(medida_de_pieza == 'km' && medida_a_convertir =='m' && tipo_de_medida == 'L'){
+				return medida*1000 +' m';
+				
+		}
+		else if(medida_de_pieza == 'cm' && medida_a_convertir =='m' && tipo_de_medida == 'L'){
+				return medida/100 +'m';
+				
+		}else if(medida_de_pieza == 'cm' && medida_a_convertir =='m' && tipo_de_medida == 'L'){
+				return medida/100+' m';
+				
+		}else if(medida_de_pieza == 'F' && medida_a_convertir =='C' && tipo_de_medida == 'T'){
+				return (medida-32)/1.8+' C';
+				
+		}
+		else if(medida_de_pieza == 'C' && medida_a_convertir =='F' && tipo_de_medida == 'T'){
+				return (medida*1.8)+32+' F';
+				
+		}else if(medida_de_pieza == 'K' && medida_a_convertir =='F' && tipo_de_medida == 'T'){
+				return ((medida-273.15)*1.8)+32 +' F';
+				
+		}
+		else if(medida_de_pieza == 'F' && medida_a_convertir =='K' && tipo_de_medida == 'T'){
+				return ((medida-32)/1.8)+273.15 +' K';
+				
+		}
+		else if(medida_de_pieza == 'C' && medida_a_convertir =='K' && tipo_de_medida == 'T'){
+				return medida+273.15+' K';
+				
+		}
+		else if(medida_de_pieza == 'K' && medida_a_convertir =='C' && tipo_de_medida == 'T'){
+				return K-273.15+' C';
+				
+		}else{
+			console.log('no se cumplen los requerimientos de conversion, revise la medida actual, la medida a la que se desea convertir y el tipo de medida si es temperatura (T) o longitud (L)');
+		}
+
+	}
+}
+/*var tipo_de_medida = {};
+function convertir(medida,medida_de_pieza,medida_a_convertir,tipo_de_medida){
+	
+		else if(medida_de_pieza == 'cm' && medida_a_convertir =='m' && tipo_de_medida == 'L'){
+				var L = {convertir: 	medida/100}
+				console.log(L.convertir);
+		}else if(medida_de_pieza == 'cm' && medida_a_convertir =='m' && tipo_de_medida == 'L'){
+				var L = {convertir: 	medida/100}
+				console.log(L.convertir);
+		}else if(medida_de_pieza == 'cm' && medida_a_convertir =='m' && tipo_de_medida == 'L'){
+				var L = {convertir: 	medida/100}
+				console.log(L.convertir);
+		}else if(medida_de_pieza == 'cm' && medida_a_convertir =='m' && tipo_de_medida == 'L'){
+				var L = {convertir: 	medida/100}
+				console.log(L.convertir);
+		}else if(medida_de_pieza == 'cm' && medida_a_convertir =='m' && tipo_de_medida == 'L'){
+				var L = {convertir: 	medida/100}
+				console.log(L.convertir);
+		}else if(medida_de_pieza == 'cm' && medida_a_convertir =='m' && tipo_de_medida == 'L'){
+				var L = {convertir: 	medida/100}
+				console.log(L.convertir);
+		}
+			
+	}
+	*/
+
